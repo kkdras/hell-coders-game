4e3242026f9d59075811f7b1f7383d8c5e2560d4
Tatiana Tikhonova <tanianova@yandex.ru>
2023-05-01 20:08:04 +0300

    merge develop

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -4,8 +4,8 @@ import { Routes, Route } from 'react-router-dom'
 import { Layout } from './components/layout'
 import { Home } from './pages/Home/Home'
 import { Game } from './pages/Game/Game'
-import { Error500 } from './pages/error500'
-import { Error404 } from './pages/error404'
+import { Error500 } from './pages/Error500/Error500'
+import { Error404 } from './pages/Error404/Error404'
 
 export enum RouteNames {
   MAIN = '/',
@@ -13,9 +13,8 @@ export enum RouteNames {
   BLOG = '/blog',
   LEADERBOARD = '/leaderboard',
   PROFILE = '/profile',
+  ERROR_500 = '/error500',
 }
-
-
 
 function App() {
   useEffect(() => {
@@ -33,7 +32,7 @@ function App() {
     <Routes>
       <Route path={RouteNames.MAIN} element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/error500" element={<Error500 />} />
+        <Route path={RouteNames.ERROR_500} element={<Error500 />} />
         <Route path="*" element={<Error404 />} />
         <Route path={RouteNames.GAME} element={<Game />} />
       </Route>
