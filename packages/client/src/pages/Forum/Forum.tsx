f89c08d0d35841a8ed29dbc11a6f2e2509a04e02
StepanenkoOlgaV <stepanenko.olga@russianpost.ru>
2023-05-02 15:22:32 +0300

    Готово в одном файле

--- packages/client/src/pages/Forum/Forum.tsx
+++ packages/client/src/pages/Forum/Forum.tsx
@@ -5,17 +5,28 @@ import {
     AccordionSummary,
     AccordionDetails,
     IconButton,
+    Table,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
+    Box,
 } from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AddIcon from '@mui/icons-material/Add';
 import { useNavigate } from 'react-router-dom'
-import { forumList, themesExample } from './const'
+import { messages, themes, forums } from './const'
+import { ArrowBack } from '@mui/icons-material';
+
+
 
 
 export const Forum = () => {
-    const navigate = useNavigate()
-    return (<>
-        <Grid container spacing={2} pt={16} pb={4} color={'blue'}>
+    const navigate = useNavigate();
+
+    return (<Box pt={4}>
+        <ArrowBack onClick={() => { navigate(-1) }} />
+        <Grid container spacing={2} pt={12} pb={4} color={'blue'}>
             <Grid item xs={8} >
                 <Typography pl={2} >ФОРУМЫ</Typography>
             </Grid>
@@ -28,30 +39,29 @@ export const Forum = () => {
         </Grid>
 
         {
-            forumList.map(item => (
-                <Accordion>
+            forums.map(forum => (
+                <Accordion sx={{ backgroundColor: '#e3f2fd', borderRadius: '5%' }} key={forum.id}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1a-content"
-                        border-radius="0px"
+
                     >
                         <Grid container spacing={2} color={'text.primary'}>
                             <Grid item xs={9} >
-                                <Typography pt={1}>{item.title}</Typography>
+                                <Typography pt={1}>{forum.title}</Typography>
                             </Grid>
                             <Grid item xs={1} >
-                                <Typography pt={1}>{item.themesCount}</Typography>
+                                <Typography pt={1}>{forum.themesCount}</Typography>
                             </Grid>
                             <Grid item xs={1} >
                                 <IconButton color="success" onClick={(e) => {
-                                    e.preventDefault();
                                     console.log('clicked');
                                 }}>
                                     <AddIcon />
                                 </IconButton>
                             </Grid>
                             <Grid item xs={1} >
-                                <Typography pt={1} >{item.answers}</Typography>
+                                <Typography pt={1} >{forum.answers}</Typography>
                             </Grid>
                         </Grid>
                     </AccordionSummary>
@@ -67,25 +77,57 @@ export const Forum = () => {
                                 <Typography pl={10} variant="body2">Последний</Typography>
                             </Grid>
                         </Grid>
-                        {themesExample.map(item => (
-                            <Accordion>
+                        {themes.map(theme => (
+                            <Accordion sx={{ backgroundColor: '#e3f2fd', borderRadius: '5%' }} key={theme.id}>
                                 <AccordionSummary
                                     expandIcon={<ExpandMoreIcon />}
                                     aria-controls="panel1a-content"
-                                    border-radius="0px"
                                 >
                                     <Grid container spacing={2} color={'text.primary'}>
                                         <Grid item xs={9} >
-                                            <Typography >{item.title}</Typography>
+                                            <Typography >{theme.title}</Typography>
                                         </Grid>
                                         <Grid item xs={1} >
-                                            <Typography >{item.answers}</Typography>
+                                            <Typography >{theme.answers}</Typography>
                                         </Grid>
                                         <Grid item xs={2} >
-                                            <Typography pl={2}>{item.lastAnswer}</Typography>
+                                            <Typography pl={2}>{theme.lastAnswer}</Typography>
                                         </Grid>
                                     </Grid>
                                 </AccordionSummary>
+                                <AccordionDetails>
+                                    <Table stickyHeader aria-label="sticky table">
+                                        <TableHead >
+                                            <TableRow >
+                                                <TableCell align="center" colSpan={2} sx={{ color: 'blue' }}>
+                                                    Сообщение
+                                                </TableCell>
+                                                <TableCell align="center" colSpan={2} sx={{ color: 'blue' }}>
+                                                    Автор
+                                                </TableCell>
+                                                <TableCell align="center" colSpan={2} sx={{ color: 'blue' }} >
+                                                    Время
+                                                </TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody sx={{ backgroundColor: '#ffab91' }}>
+                                            {messages.map(message => (
+                                                <TableRow key={message.id}>
+                                                    <TableCell align="left" colSpan={2}>
+                                                        {message.text}
+                                                    </TableCell>
+                                                    <TableCell align="center" colSpan={2}>
+                                                        {message.author}
+                                                    </TableCell>
+                                                    <TableCell align="center" colSpan={2}>
+                                                        {message.time}
+                                                    </TableCell>
+                                                </TableRow>
+                                            ))}
+
+                                        </TableBody>
+                                    </Table>
+                                </AccordionDetails>
                             </Accordion>
                         ))
                         }
@@ -93,8 +135,6 @@ export const Forum = () => {
                 </Accordion>
             ))
         }
-
-
-    </>
+    </Box>
     )
 }
--- packages/client/src/pages/Forum/const.ts
+++ packages/client/src/pages/Forum/const.ts
@@ -1,33 +1,58 @@
-export const forumList = [{
+export const forums = [{
+  id: 1,
   title: "Тетрисы мира",
   themesCount: 222,
   answers: 1222,
 },
 {
+  id: 2,
   title: "Технологии",
   themesCount: 2,
   answers: 12,
 },
 {
+  id: 3,
   title: "Флудилка",
   themesCount: 222,
   answers: 1222,
 },
 ];
 
-export const themesExample = [{
+export const themes = [{
+  id: 4,
   title: "Советский тетрис",
   answers: 45,
   lastAnswer: "02.05.2023"
 },
 {
+  id: 5,
   title: "Японский тетрис",
   answers: 4,
   lastAnswer: "03.05.2023"
 },
 {
+  id: 6,
   title: "Древнеегипетский тетрис",
   answers: 100,
   lastAnswer: "23.12.2022"
+},
+];
+
+export const messages = [{
+  text: "В советском союзе тетриса не было!",
+  author: "Эксперт",
+  time: "02.05.2023 13:00"
+},
+{
+  id: 7,
+  text: "Ты просто не знаешь, кто его создал",
+  author: "Волшебник",
+  time: "02.05.2023 14:00"
+},
+{
+  id: 8,
+  text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibu",
+  author: "Прохожий",
+  time: "02.05.2023 15:00"
 },
 ];
