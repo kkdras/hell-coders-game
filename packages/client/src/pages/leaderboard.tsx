5b1a83676c8a1aba5599a055b25e5119e64f55dd
odminbot <odminbot@gmail.com>
2023-05-02 17:23:12 +0300

    finish leaderboard page

--- packages/client/src/pages/leaderboard.tsx
+++ packages/client/src/pages/leaderboard.tsx
@@ -1,82 +1,55 @@
-import { Box, Grid, Paper } from '@mui/material'
-import { styled } from '@mui/material/styles'
-import testAvatar from '../image/avatar1.png'
 import { useEffect } from 'react'
+import { Box } from '@mui/material'
+import testAvatar from '../image/avatar1.png'
+import { DataGrid, GridCellParams } from '@mui/x-data-grid'
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    minWidth: '50px',
-  }));
+const columns = [
+    { field: 'id', 
+      headerName: 'ID', 
+      width: 80,
+    },
+    {
+      field: 'avatar',
+      headerName: 'Avatar',
+      sortable: false,
+      width: 80,
+      renderCell: (params: GridCellParams) =>  (
+            <img 
+                src={params.value as string} 
+                style={{ width: 37, borderRadius: '50%' }}
+            />
+        ),
+    },
+    {
+      field: 'name',
+      headerName: 'Name',
+      width: 150,
+    },
+    {
+      field: 'points',
+      headerName: 'Points',
+      type: 'number',
+      width: 100,
+    },
+  ];
 
-const Users = [
-    {
-        id: 1,
-        avatar: '',
-        name: 'Vasya',
-        points: 10000,
-    },
-    {
-        id: 2,
-        avatar: '',
-        name: 'Petya',
-        points: 9000,
-    },
-    {
-        id: 3,
-        avatar: '',
-        name: 'Masha',
-        points: 8000,
-    },
-    {
-        id: 4,
-        avatar: '',
-        name: 'Tetya',
-        points: 7000,
-    },
-    {
-        id: 5,
-        avatar: '',
-        name: 'Motya',
-        points: 6000,
-    },
-    {
-        id: 6,
-        avatar: '',
-        name: 'Ira',
-        points: 5000,
-    },
-    {
-        id: 7,
-        avatar: '',
-        name: 'Dima',
-        points: 4000,
-    },
-    {
-        id: 8,
-        avatar: '',
-        name: 'Tanya',
-        points: 3000,
-    },
-    {
-        id: 9,
-        avatar: '',
-        name: 'Olga',
-        points: 2000,
-    },
-    {
-        id: 10,
-        avatar: '',
-        name: 'Kostya',
-        points: 1000,
-    },
+const rows = [
+    { id: 1, avatar: testAvatar, name: 'Vasya', points: 1000000 },
+    { id: 2, avatar: testAvatar, name: 'Petya', points: 9000  },
+    { id: 3, avatar: testAvatar, name: 'Masha', points: 8000  },
+    { id: 4, avatar: testAvatar, name: 'Tetya', points: 7000  },
+    { id: 5, avatar: testAvatar, name: 'Motya', points: 6000  },
+    { id: 6, avatar: testAvatar, name: 'Ira',   points: 5000  },
+    { id: 7, avatar: testAvatar, name: 'Dima',  points: 4000  },
+    { id: 8, avatar: testAvatar, name: 'Tanya', points: 3000  },
+    { id: 9, avatar: testAvatar, name: 'Olga',  points: 2000  },
+    { id: 10, avatar: testAvatar, name: 'Kostya Super Long Name', points: 1000 },
+    { id: 11, avatar: testAvatar, name: 'Name on next Page', points: 500 },
 ];
 
-export function LeaderBoard() {
 
+export function LeaderBoard() {
+    
     useEffect(() => {
     document.title = 'Таблица рекордов - Тетрис'
     }, [])
@@ -90,30 +63,24 @@ export function LeaderBoard() {
             m: 1,
         }}>
             <Box sx={{ m: 1, mt: 3}}>
-                <h1>Таблица рекордов</h1>
+                <h1>Leaderboard</h1>
             </Box>
-            <Box sx={{ flexGrow: 1, m: 1, mt: 3 }}>
-                <Grid container spacing={{ xs: 1, md: 6 }} columns={{ xs:2, sm: 4, md: 8 }}>
-                    {Array.from(Users).map((_, index) => (
-                        <Grid item xs={2} sm={4} md={4} key={index}>
-                            <Grid container spacing={0.5}>
-                                <Grid item xs={1} sx={{ minWidth: '20px' }}>
-                                    <Item>{Users[index].id}</Item>
-                                </Grid>
-                                <Grid item xs={1} sx={{ minWidth: '37px' }}>
-                                    <img src={testAvatar} width={37} height={37}/>
-                                </Grid>
-                                <Grid item xs>
-                                    <Item>{Users[index].name}</Item>
-                                </Grid>
-                                <Grid item xs={2} sx={{ minWidth: '50px' }}>
-                                    <Item>{Users[index].points}</Item>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    ))}
-                </Grid>
+            <Box sx={{ m: 1, mt: 3}}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    initialState={{
+                        pagination: {
+                          paginationModel: {
+                            pageSize: 10,
+                          },
+                        },
+                      }}
+                    pageSizeOptions={[5, 10, 25]}
+                    disableRowSelectionOnClick
+                />
             </Box>
         </Box>
     );
 }
+  
