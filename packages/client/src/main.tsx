2e0242ae29386de558e04919bfa83468dc12ba81
Константин Козлов <kkdras@MacBook-Pro-Konstantin.local>
2023-05-15 11:09:54 +0300

    feature/cod-25 fix layout

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -9,13 +9,11 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
+  </BrowserRouter>
 )
