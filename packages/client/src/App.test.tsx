58738cc2652ba7a854b5a3d6478379dc1374afc9
StepanenkoOlgaV <stepanenko.olga@russianpost.ru>
2023-05-25 10:50:41 +0300

    Lint

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,5 +1,5 @@
 import App from './App'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
