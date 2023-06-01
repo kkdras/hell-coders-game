51eb89fc29351964278b2a27124dcfe1b69327cc
StepanenkoOlgaV <stepanenko.olga@russianpost.ru>
2023-06-01 07:16:18 +0300

    fix: registration without authorization

--- packages/client/src/processes/auth/Auth.tsx
+++ packages/client/src/processes/auth/Auth.tsx
@@ -1,11 +1,14 @@
 import { FC, PropsWithChildren } from 'react'
 import { useSelector } from 'react-redux'
+import { useLocation } from 'react-router-dom'
+import { Register } from '../../pages/Register/Register'
 
 import { RootState } from '../../store/rootReducer'
 import { AuthorizationForm } from './AuthorizationForm'
 
 export const Auth: FC<PropsWithChildren> = ({ children }) => {
   const { isUserAuthorized } = useSelector((state: RootState) => state.auth)
+  const location = useLocation();  
 
-  return isUserAuthorized ? <>{children}</> : <AuthorizationForm />
+  return isUserAuthorized || location.pathname === "/register" ? <>{children}</> : <AuthorizationForm />
 }
