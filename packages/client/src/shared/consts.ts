2e7eb05fd3e19f73f943a4e6fc79feb2feed6307
StepanenkoOlgaV <stepanenko.olga@russianpost.ru>
2023-06-21 17:11:26 +0300

    Промежуточный коммит

--- packages/client/src/shared/consts.ts
+++ packages/client/src/shared/consts.ts
@@ -1,2 +1,2 @@
 export const YANDEX_BASE_URL = 'https://ya-praktikum.tech/api/v2'
-export const CUSTOM_BASE_URL = 'http://localhost:8080/api'
+export const CUSTOM_BASE_URL = 'http://localhost:3001/api'
