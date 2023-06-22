910b413b94e525294208d29c8e14b706d50f44a7
StepanenkoOlgaV <stepanenko.olga@russianpost.ru>
2023-06-22 13:39:37 +0300

    из аккордеона вынесены плюсики

--- packages/client/src/pages/Forum/components/CommentAccordeon/CommentAccordeon.tsx
+++ packages/client/src/pages/Forum/components/CommentAccordeon/CommentAccordeon.tsx
@@ -22,77 +22,80 @@ import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../../../../store/rootReducer'
 import { AppStoreDispatch } from '../../../../store'
 import { getCommentReplyes } from '../../../../store/forum/actions'
+import { commentReplyes } from '../../const'
 
 export function CommentAccordeon(comment: IComment) {
   const lightOrange = deepOrange[400]
   const [showAddReply, setShowAddReply] = useState<boolean>(false)
   const dispatch = useDispatch<AppStoreDispatch>()
-  
+
   const { user } = useSelector((state: RootState) => state.user)
 
-  const { replyes } = useSelector((state: RootState) => state.forum)
+  /* const { replyes } = useSelector((state: RootState) => state.forum)
   const  commentReplyes = replyes[comment.id]
 
 
   useEffect(() => {
     dispatch(getCommentReplyes(comment.id));       
-  }, []) 
+  }, [])  */
 
   return (
     <>
-      <Accordion sx={{ borderRadius: '5%' }} key={comment.id}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content">
-          <Grid container spacing={2} color={'text.primary'}>
-            <Grid item xs={9}>
-              <Typography>{comment.title}</Typography>
-            </Grid>
-            <Grid item xs={1}>
-              <Typography>{commentReplyes?.length}</Typography>
-            </Grid>
-          </Grid>
-          <Grid item xs={1}>
-            <IconButton
-              color="success"
-              onClick={e => {
-                e.preventDefault()
-                setShowAddReply(true)
-              }}>
-              <AddIcon />
-            </IconButton>
-          </Grid>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="center" colSpan={2} sx={{ color: 'blue' }}>
-                  Сообщение
-                </TableCell>
-                <TableCell align="center" colSpan={2} sx={{ color: 'blue' }}>
-                  Автор
-                </TableCell>
-                <TableCell align="center" colSpan={2} sx={{ color: 'blue' }}>
-                  Время
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            {commentReplyes.length &&
-              <TableBody sx={{ backgroundColor: lightOrange }}>
-                {commentReplyes.map(
-                  reply =>
-                  (
-                    <ReplyesTable
-                      key={reply.id}
-                      {...reply}
-                    />
-                  )
-                )}
-              </TableBody>}
-          </Table>
-        </AccordionDetails>
-      </Accordion>
+      <Grid container spacing={2}>
+        <Grid item xs={11}>
+          <Accordion sx={{ borderRadius: '5%' }} key={comment.id}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content">
+              <Grid container spacing={2} color={'text.primary'}>
+                <Grid item xs={9}>
+                  <Typography>{comment.title}</Typography>
+                </Grid>
+                <Grid item xs={1}>
+                  <Typography>{commentReplyes?.length}</Typography>
+                </Grid>
+              </Grid>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Table stickyHeader aria-label="sticky table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center" colSpan={2} sx={{ color: 'blue' }}>
+                      Сообщение
+                    </TableCell>
+                    <TableCell align="center" colSpan={2} sx={{ color: 'blue' }}>
+                      Автор
+                    </TableCell>
+                    <TableCell align="center" colSpan={2} sx={{ color: 'blue' }}>
+                      Время
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                {commentReplyes.length &&
+                  <TableBody sx={{ backgroundColor: lightOrange }}>
+                    {commentReplyes.map(
+                      reply =>
+                      (
+                        <ReplyesTable
+                          key={reply.id}
+                          {...reply}
+                        />
+                      )
+                    )}
+                  </TableBody>}
+              </Table>
+            </AccordionDetails>
+          </Accordion></Grid>
+        <Grid item xs={1}>
+          <IconButton
+            color="success"
+            onClick={e => {
+              e.preventDefault()
+              setShowAddReply(true)
+            }}>
+            <AddIcon />
+          </IconButton>
+        </Grid></Grid>
       {showAddReply && user && <AddReply showAddReply={showAddReply} setShowAddReply={setShowAddReply} commentId={comment.id} authorLogin={user?.login} />}
     </>
   )
--- packages/client/src/pages/Forum/components/TopicAccordeon/TopicAccordeon.tsx
+++ packages/client/src/pages/Forum/components/TopicAccordeon/TopicAccordeon.tsx
@@ -17,82 +17,85 @@ import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../../../../store/rootReducer'
 import { AppStoreDispatch } from '../../../../store'
 import { getTopicComments } from '../../../../store/forum/actions'
+import { topicComments } from '../../const'
 
 export function TopicAccordeon(topic: ITopic) {
   const lightLightBlue = lightBlue[50]
   const [showAddComment, setShowAddComment] = useState<boolean>(false)
   const dispatch = useDispatch<AppStoreDispatch>()
 
-  const { comments } = useSelector((state: RootState) => state.forum)
-  const topicComments = comments[topic.id]
-
-  useEffect(() => {
-    dispatch(getTopicComments(topic.id))
-  }, [])
+  /*  const { comments } = useSelector((state: RootState) => state.forum)
+   const topicComments = comments[topic.id]
+ 
+   useEffect(() => {
+     dispatch(getTopicComments(topic.id))
+   }, []) */
 
 
   return (
     <>
-      <Accordion
-        sx={{ backgroundColor: lightLightBlue, borderRadius: '5%' }}
-        key={topic.id}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content">
-          <Grid container spacing={2} color={'text.primary'}>
-            <Grid item xs={9}>
-              <Typography pt={1}>{topic.title}</Typography>
-            </Grid>
-            <Grid item xs={1}>
-              <Typography pt={1}>{topicComments.length}</Typography>
-            </Grid>
-          </Grid>
-          <Grid item xs={1}>
-            <IconButton
-              color="success"
-              onClick={e => {
-                e.preventDefault()
-                setShowAddComment(true)
-              }}>
-              <AddIcon />
-            </IconButton>
-          </Grid>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Grid
-            container
-            spacing={2}
-            pb={2}
-            color={'text.secondary'}
-            fontStyle={'italic'}>
-            <Grid item xs={8}>
-              <Typography pl={2} variant="body2">
-                Комментарии
-              </Typography>
-            </Grid>
-            <Grid item xs={1}>
-              <Typography pl={8} variant="body2">
-                Ответы
-              </Typography>
-            </Grid>
-            <Grid item xs={2}>
-              <Typography pl={10} variant="body2">
-                Последний
-              </Typography>
-            </Grid>
-          </Grid>
-          {topicComments.map(
-            comment =>
-              <CommentAccordeon
-                key={comment.id}
-                {...comment}
-              />
-          )
-          }
-        </AccordionDetails>
-      </Accordion>
+      <Grid container spacing={2} color={'text.primary'}>
+        <Grid item xs={11}>
+          <Accordion
+            sx={{ backgroundColor: lightLightBlue, borderRadius: '5%' }}
+            key={topic.id}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content">
+              <Grid container spacing={2} color={'text.primary'}>
+                <Grid item xs={9}>
+                  <Typography pt={1}>{topic.title}</Typography>
+                </Grid>
+                <Grid item xs={1}>
+                  <Typography pt={1}>{topicComments.length}</Typography>
+                </Grid>
+              </Grid>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Grid
+                container
+                spacing={2}
+                pb={2}
+                color={'text.secondary'}
+                fontStyle={'italic'}>
+                <Grid item xs={8}>
+                  <Typography pl={2} variant="body2">
+                    Комментарии
+                  </Typography>
+                </Grid>
+                <Grid item xs={1}>
+                  <Typography pl={8} variant="body2">
+                    Ответы
+                  </Typography>
+                </Grid>
+                <Grid item xs={2}>
+                  <Typography pl={10} variant="body2">
+                    Последний
+                  </Typography>
+                </Grid>
+              </Grid>
+              {topicComments.map(
+                comment =>
+                  <CommentAccordeon
+                    key={comment.id}
+                    {...comment}
+                  />
+              )
+              }
+            </AccordionDetails>
+          </Accordion>
+        </Grid>
+        <Grid item xs={1}>
+          <IconButton
+            color="success"
+            onClick={e => {
+              e.preventDefault()
+              setShowAddComment(true)
+            }}>
+            <AddIcon />
+          </IconButton>
+        </Grid></Grid>
       {showAddComment && <AddComment showAddComment={showAddComment} setShowAddComment={setShowAddComment} topicId={topic.id} />}
-
     </>
   )
 }
