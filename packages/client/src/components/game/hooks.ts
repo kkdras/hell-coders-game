eae537325df5709802040e3658f669f4614e4bb5
Константин Козлов <kkdras@MacBook-Pro-Konstantin.local>
2023-06-25 01:27:12 +0300

    COD-35 fix game speed

--- packages/client/src/components/game/hooks.ts
+++ packages/client/src/components/game/hooks.ts
@@ -84,7 +84,7 @@ export const useGame = (canvasRef: RefObject<HTMLCanvasElement>) => {
       canvas: canvasRef.current as HTMLCanvasElement,
       cellSize: 34,
       // двигаем фигуру каждые 800 милисекунд
-      movesBoundary: 50,
+      movesBoundary: 800,
     })
   }, [])
 
