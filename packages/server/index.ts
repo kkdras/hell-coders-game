057be1617b1d023f0bebe9eadd52b8163989adf0
Константин Козлов <kkdras@MacBook-Pro-Konstantin.local>
2023-06-28 16:44:15 +0300

    feature/cod-42 upsate server

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,13 +1,13 @@
+import { forumRouter } from './routes/forum.routes'
 import { userRouter } from './routes/user.routes'
 import dotenv from 'dotenv'
 import cors from 'cors'
-dotenv.config()
 import bodyParser from 'body-parser'
 import express from 'express'
 import { themeRouter } from './routes/theme.routes'
-import { topicRouter } from './routes/topic.routes'
 import { sequelize } from './models'
-import { commentRouter } from './routes/comment.routes'
+
+dotenv.config()
 
 const app = express()
 app.use(cors())
@@ -27,10 +27,7 @@ sequelize
     console.log('Failed to sync db: ' + err.message)
   })
 
-app.use('/api/forum', topicRouter)
-app.use('/api/theme', themeRouter)
-app.use('/api/user', userRouter)
-app.use('/api/comment', commentRouter)
+app.use('/api/', forumRouter, themeRouter, userRouter)
 
 app.get('/', (_, res) => {
   res.json('👋 Howdy from the server :)')
