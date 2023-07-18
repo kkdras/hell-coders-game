12b7fe4a97ce571aad4be423e54ec92a721cd703
StepanenkoOlgaV <stepanenko.olga@russianpost.ru>
2023-07-18 09:19:18 +0300

    redirect_uri

--- packages/client/src/processes/auth/const.ts
+++ packages/client/src/processes/auth/const.ts
@@ -5,5 +5,5 @@ export const defaultValues: SignInRequest = {
   password: ''
 }
 
-//export const redirect_uri = `http://hell-coders-tetris-25.ya-praktikum.tech`
-export const redirect_uri = `http://localhost:3000`
+export const redirect_uri = `http://hell-coders-tetris-25.ya-praktikum.tech`
+
